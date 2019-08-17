4cb9077f527d160755f924054f31d92b8f45f61f
Valter Negreiros <valternegreiros@dvitsolutions.com>
2019-08-17 17:50:15 -0300

    Implementado Cadastrar Aluno

--- src/main/java/com/adverum/vagnernegreiros/cursos/controller/ContratoController.java
+++ src/main/java/com/adverum/vagnernegreiros/cursos/controller/ContratoController.java
@@ -1,18 +1,18 @@
 package com.adverum.vagnernegreiros.cursos.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.adverum.vagnernegreiros.cursos.repository.AlunoRepository;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ContratoController {
 
 	@RequestMapping(value = "/contrato/list-contrato" ,method = RequestMethod.GET)
-	public String list() {		
-		return "contrato/list-contrato";
+	public ModelAndView list() {		
+		ModelAndView model = new ModelAndView("contrato/list-contrato");
+		
+		return model;
 	}
 	
 	
--- src/main/java/com/adverum/vagnernegreiros/cursos/repository/AlunoRepository.java
+++ src/main/java/com/adverum/vagnernegreiros/cursos/repository/AlunoRepository.java
@@ -1,8 +1,12 @@
 package com.adverum.vagnernegreiros.cursos.repository;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.adverum.vagnernegreiros.cursos.model.Aluno;
@@ -10,6 +14,9 @@ import com.adverum.vagnernegreiros.cursos.model.Aluno;
 @Repository
 @Transactional
 public interface AlunoRepository extends CrudRepository<Aluno, Long>{
+	
+	@Query("SELECT a FROM Aluno a WHERE a.cpf=:cpf or a.email=:email")
+    List<Aluno> findByCPFOrEmail(@Param("cpf") String cpf,@Param("email") String email);
 
 }
 
