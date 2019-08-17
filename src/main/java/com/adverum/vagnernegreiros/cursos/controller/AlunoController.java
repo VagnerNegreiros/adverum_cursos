913935245162d7001bf01c32fce203f8defab052
Vagner Negreiros <vagnerlimanegreiros@gmail.com>
2019-08-17 18:27:50 -0300

    Implementado Editar Aluno

--- src/main/java/com/adverum/vagnernegreiros/cursos/controller/AlunoController.java
+++ src/main/java/com/adverum/vagnernegreiros/cursos/controller/AlunoController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -81,11 +82,42 @@ public class AlunoController {
 		return model;
 	}
 	
-	@RequestMapping(value = "/aluno/edit-aluno" ,method = RequestMethod.GET)
-	public ModelAndView editAlunoPage(Aluno aluno) {	
+	@RequestMapping(value = "/aluno/edit-aluno/{id}" ,method = RequestMethod.GET)
+	public ModelAndView editAlunoPage(@PathVariable("id") String id) {	
 		ModelAndView model = new ModelAndView("aluno/edit-aluno");
+		
+		Long idAluno = Long.parseLong(id);		
+		
+		Aluno aluno = alunoRepository.findById(Long.parseLong(id)).get();
 		   
         model.addObject("aluno", aluno);
+		
+		return model;
+	}
+	
+	@PostMapping("/aluno/editAluno")
+	public ModelAndView editAluno(@Valid Aluno aluno, BindingResult result) {
+		
+		ModelAndView model = new ModelAndView("aluno/list-aluno");
+		
+		if(result.hasErrors()) {
+			model.setViewName("aluno/create-aluno");			
+	        model.addObject("showError", true);
+	        model.addObject("error", "Preencha todos os campos corretamente");
+			return model;
+		}
+		
+		alunoRepository.save(aluno);
+		
+		// Create an empty list 
+        List<Aluno> listAluno = new ArrayList<>(); 
+  
+        alunoRepository.findAll().forEach(listAluno::add);         
+		
+        model.addObject("listAluno", listAluno);
+        model.addObject("showSuccess", true);
+        model.addObject("message", "Aluno atualizado com sucesso!");
+        
 		
 		return model;
 	}
