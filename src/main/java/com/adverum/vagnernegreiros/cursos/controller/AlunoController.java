4e0db8e51171b2d2be42e80e3cf8bd823fff4fa8
Valter Negreiros <valternegreiros@dvitsolutions.com>
2019-08-17 17:50:15 -0300

    Implementado Cadastrar Aluno

--- src/main/java/com/adverum/vagnernegreiros/cursos/controller/AlunoController.java
+++ src/main/java/com/adverum/vagnernegreiros/cursos/controller/AlunoController.java
@@ -1,10 +1,19 @@
 package com.adverum.vagnernegreiros.cursos.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.adverum.vagnernegreiros.cursos.model.Aluno;
 import com.adverum.vagnernegreiros.cursos.repository.AlunoRepository;
 
 @Controller
@@ -14,9 +23,71 @@ public class AlunoController {
 	private AlunoRepository alunoRepository;
 
 	@RequestMapping(value = "/aluno/list-aluno" ,method = RequestMethod.GET)
-	public String list() {		
-		return "aluno/list-aluno";
+	public ModelAndView list() {	
+		ModelAndView model = new ModelAndView("aluno/list-aluno");
+		
+		// Create an empty list 
+        List<Aluno> listAluno = new ArrayList<>(); 
+  
+        alunoRepository.findAll().forEach(listAluno::add);         
+		
+        model.addObject("listAluno", listAluno);
+		
+		return model;
 	}
 	
+	@RequestMapping(value = "/aluno/create-aluno" ,method = RequestMethod.GET)
+	public ModelAndView createAlunoPage() {	
+		ModelAndView model = new ModelAndView("aluno/create-aluno");
+		
+		Aluno aluno = new Aluno();
+		   
+        model.addObject("aluno", aluno);
+		
+		return model;
+	}
+	
+	@PostMapping("/aluno/createAluno")
+	public ModelAndView save(@Valid Aluno aluno, BindingResult result) {
+		
+		ModelAndView model = new ModelAndView("aluno/list-aluno");
+		
+		if(result.hasErrors()) {
+			model.setViewName("aluno/create-aluno");			
+	        model.addObject("showError", true);
+	        model.addObject("error", "Preencha todos os campos corretamente");
+			return model;
+		}
+		
+		if(!alunoRepository.findByCPFOrEmail(aluno.getCpf(), aluno.getEmail()).isEmpty()) {
+			model.setViewName("aluno/create-aluno");			
+	        model.addObject("showError", true);
+	        model.addObject("error", "Já existe um aluno com cpf ou email cadastrado");
+			return model;
+		};
+		
+		alunoRepository.save(aluno);
+		
+		// Create an empty list 
+        List<Aluno> listAluno = new ArrayList<>(); 
+  
+        alunoRepository.findAll().forEach(listAluno::add);         
+		
+        model.addObject("listAluno", listAluno);
+        model.addObject("showSuccess", true);
+        model.addObject("message", "Aluno cadastrado com sucesso!");
+        
+		
+		return model;
+	}
+	
+	@RequestMapping(value = "/aluno/edit-aluno" ,method = RequestMethod.GET)
+	public ModelAndView editAlunoPage(Aluno aluno) {	
+		ModelAndView model = new ModelAndView("aluno/edit-aluno");
+		   
+        model.addObject("aluno", aluno);
+		
+		return model;
+	}
 	
 }
